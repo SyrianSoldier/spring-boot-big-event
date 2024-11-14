a17f0ccce92690325bba5227f24b4966761b4175
Summer <summer@tradev-group.com>
2024-11-14 10:37:40 +0800

    feat: 编写sql, 创建数据库和用户表
    笔记:
    1. tinyint指0-255的整数
    2. unsigned是修饰整数的, 必须放在数据类型后边. 如 int unsigned, 不能是 unsigned int
    3. now() 是mysql提供的一个函数, 返回当前的时间, 格式为YYYY-MM-DD hh:mm:ss
    4. datetime的格式为 YYYY-MM-DD hh:mm:ss
    5. unique的意思是, 该数据值不允许重复

---
+++ big-event-backend/sql/bigevent.sql
@@ -0,0 +1,29 @@
+-- 创建数据库
+drop database if exists `big_event`;
+create database `big_event`;
+
+use `big_event`;
+
+ -- 用户表
+drop table if exists `user`; 
+
+create table `user`(
+  user_id             int unsigned        primary key auto_increment              comment '用户id',
+  username            varchar(30)         not null unique                         comment '用户名',
+  password            varchar(32)         not null                                comment '密码',
+  nickname            varchar(30)         not null                                comment '昵称',
+  email               varchar(30)         not null unique                         comment '邮箱',
+  user_pic            varchar(200)        not null                                comment '头像',
+  created_time        datetime            not null default now()                   comment '创建时间',
+  updated_time        datetime            not null default now() on update now()   comment '更新时间'
+) auto_increment = 1, comment = '用户表';
+
+insert into `user` (username, password, nickname, email, user_pic)
+values
+    ('john_doe', 'password123', 'John Doe', 'john@example.com', 'path/to/avatar1.jpg'),
+    ('jane_smith', 'password456', 'Jane Smith', 'jane@example.com', 'path/to/avatar2.jpg'),
+    ('alice_wang', 'password789', 'Alice Wang', 'alice@example.com', 'path/to/avatar3.jpg'),
+    ('bob_lee', 'password012', 'Bob Lee', 'bob@example.com', 'path/to/avatar4.jpg'),
+    ('charlie_liu', 'password345', 'Charlie Liu', 'charlie@example.com', 'path/to/avatar5.jpg');
+
+
